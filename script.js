62520d4fd18640f205b63b65c59ee9b110f23f67
Re Julian <julianre2021@gmail.com>
2023-04-03 16:29:55 -0300

    agregando logica

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  const $resultados = document.querySelector('#resultado')  
   let codigosLeidos = [];
 
   const busqueda = (arreglo) => {
